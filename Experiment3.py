0d1baad028407b9ad3a5fe9151b055ac402f6af2
Tory Li <tuo90515@temple.edu>
2024-09-25 17:06:41 -0400

    Update Experiment3.py

--- Experiment3.py
+++ Experiment3.py
@@ -11,25 +11,25 @@ from VSASpace import VSASpace, matrix_to_vector
 # ======================================================================================================================
 # pre process
 
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_1_university_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_2_musicalwork_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_3_airport_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_4_building_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_5_athlete_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_6_politician_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_7_company_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_8_celestialbody_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_9_astronaut_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_10_comicscharacter_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_11_meanoftransportation_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_12_monument_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_13_food_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_14_writtenwork_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_15_sportsteam_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_16_city_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_17_artist_ground_truth.jsonl"
-# package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_18_scientist_ground_truth.jsonl"
-package_file = "/Users/tory/Downloads/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_19_film_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_1_university_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_2_musicalwork_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_3_airport_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_4_building_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_5_athlete_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_6_politician_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_7_company_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_8_celestialbody_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_9_astronaut_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_10_comicscharacter_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_11_meanoftransportation_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_12_monument_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_13_food_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_14_writtenwork_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_15_sportsteam_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_16_city_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_17_artist_ground_truth.jsonl"
+# package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_18_scientist_ground_truth.jsonl"
+package_file = "/Text2KGBench-main/data/dbpedia_webnlg/ground_truth/ont_19_film_ground_truth.jsonl"
 
 g_entities = set()
 g_relations = set()
@@ -63,7 +63,6 @@ def text_to_embedding(text, llm):
 
 
 # text2embedding("I love ITZY.", model)
-
 
 # ======================================================================================================================
 # lens definition
@@ -134,8 +133,6 @@ lr = 0.001
 epoch_VSA = 10
 
 l_num_proportion = 0.2
-# g_k_proportion = 0.2
-# g_k_bias = 0.5
 
 # VSA definition
 keyword = ["_H", "_R", "_T"]
